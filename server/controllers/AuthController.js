9473cd29805045d6094ebb61daf21df308478f02
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-06-04 18:02:45 +0530

    Auth, User & Post Controller

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,57 @@
-export const registerUser = () => {};
+import userModel from "../models/userModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const loginUser = () => {};
+// Register new user
+export const registerUser = async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+  const newUser = new userModel(req.body);
+  const { username } = req.body;
+
+  try {
+    const oldUser = await userModel.findOne({ username });
+    if (oldUser) {
+      return res.status(400).json({ message: "User Already exists" });
+    }
+
+    const user = await newUser.save();
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      process.env.JWT,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Login user
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await userModel.findOne({ username: username });
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+      if (!validity) {
+        res.status(400).json("Incorrect credentials");
+      } else {
+        const token = jwt.sign(
+          { username: user.username, id: user._id },
+          process.env.JWT,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ user, token });
+      }
+    } else {
+      res.status(404).json("User not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -1,11 +1,121 @@
-export const createPost = () => {};
+import postModel from "../models/postModel";
+import userModel from "../models/userModel";
+import mongoose from "mongoose";
 
-export const getPost = () => {};
+// create post
+export const createPost = async (req, res) => {
+  const newPost = new postModel(req.body);
+  try {
+    await newPost.save();
+    res.status(200).json(newPost);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
 
-export const updatePost = () => {};
+// get a post
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const post = await postModel.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
 
-export const deletePost = () => {};
+// update post
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await postModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post Updated!");
+    } else {
+      res.status(403).json("Authentication failed");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
 
-export const likePost = () => {};
+// delete a post
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await postModel.findById(id);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json("Post Deleted.");
+    } else {
+      res.status(403).json("Action forbidden");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
 
-export const getTimelinePosts = () => {};
+// like or dislike a post
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await postModel.findById(id);
+    if (post.likes.includes(userId)) {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json("Post disliked");
+    } else {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json("Post liked");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const currentUserPosts = await postModel.find({ userId: userId });
+    const followingPosts = await userModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return new Date(b.createdAt) - new Date(a.createdAt);
+        })
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
