dbe90d24465aefd408bbdf7aadf03b9d0ae48d38
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-06-04 15:31:23 +0530

    Auth, User & Post Routes

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,3 @@
+export const registerUser = () => {};
+
+export const loginUser = () => {};
---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+export const getUser = () => {};
+
+export const getAllUsers = () => {};
+
+export const updateUser = () => {};
+
+export const deleteUser = () => {};
+
+export const followUser = () => {};
+
+export const unfollowUser = () => {};
