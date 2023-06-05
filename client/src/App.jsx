9ac5e74ab212e944b4a7098ed6c2ad4bb4c582a3
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-06-05 18:56:43 +0530

    Home page & Search Component Design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Home from "./pages/home/home";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="App">
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <Home />
+    </div>
   );
 }
 
